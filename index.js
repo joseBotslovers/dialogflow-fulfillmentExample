25fa571dd49fbc8c0f93a1c86408c385b4649785
José <veganum@hotmail.com>
2020-11-23 22:58:07 +0100

    segundo commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+const dfff = require('dialogflow-fulfillment');
+
+//Mensaje principal endpoint
+app.get('/',(req,res)=>{
+    res.send("¡¡En directoo!!")
+});
+
+
+app.post('/',express.json(), (req,res)=>{
+    const agent = new dfff.WebhookClient({
+        request: req,
+        response: res
+    });
+
+    //funcion mensaje
+    function demo(agent){
+        agent.add("Enviando respuesta al Wekhook server");
+    }
+
+    //funcion mensaje enriquecido
+    function customPayloadDemo(agent){
+        var payloadData = {
+                "richContent": [
+                  [
+                    {
+                      "type": "accordion",
+                      "title": "Accordion titulo",
+                      "subtitle": "Accordion subtitulo",
+                      "image": {
+                        "src": {
+                          "rawUrl": "https://example.com/images/logo.png"
+                        }
+                      },
+                      "text": "Aqui se pone el texto "
+                    }
+                  ]
+                ] 
+        }
+
+        agent.add ( new dfff.Payload(agent.UNSPECIFIED , payloadData,{
+           sendAsMessage: true,
+           rawPayload: true 
+        }))      
+    }
+
+    var intentMap = new Map();
+
+    //Intent y funcion para interactuar
+    intentMap.set('webhookDemo', demo);
+    intentMap.set('customPayloadDemo', customPayloadDemo);
+
+    //
+    agent.handleRequest(intentMap);
+
+})
+
+// ARRANCAMOS EL SERVICIO
+const port = 3000;
+const dateTime = Date.now();
+
+app.listen(process.env.PORT || port, function () {
+  console.log('\x1b[36m'+'------------------------------------------------ ');
+  console.log('\x1b[35m'+'          ** Puerto '+(process.env.PORT || port +' activo **'));
+  console.log('\x1b[0m'+" [ "+Date()+" ] ");
+  console.log(`  Ejemplo escuchando en: ( http://localhost:${port} )`);
+  console.log('\x1b[36m'+'------------------------------------------------ ');
+  console.log('\x1b[0m');
+});
+//Correr en nodemon --> npm run dev
