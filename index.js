f0a58d3922139adccdfdda8bd96908a9ce38e18c
José <veganum@hotmail.com>
2020-11-25 13:44:02 +0100

    mas cambios

--- index.js
+++ index.js
@@ -1,69 +1,100 @@
 const express = require('express');
 const app = express();
 const dfff = require('dialogflow-fulfillment');
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+        user: 'jose@botslovers.com',
+        pass: '211188-c'
+    }
+});
 
 //Mensaje principal endpoint
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     //res.send("¡¡En directoo!!")
-   res.sendFile('index.html', { root : __dirname})
- 
+    res.sendFile('index.html', { root: __dirname })
+
 });
 
 
-app.post('/',express.json(), (req,res)=>{
-   
+app.post('/', express.json(), (req, res) => {
+
     const agent = new dfff.WebhookClient({
         request: req,
         response: res
     });
 
     //Ejemplo de mensaje simple
-    function demo(agent){
+    function demo(agent) {
         agent.add("Enviando respuesta al Wekhook server");
     }
 
     //Ejemplo de mensaje enriquecido
-    function customPayloadDemo(agent){
+    function customPayloadDemo(agent) {
         var payloadData = {
-                "richContent": [
-                  [
-                    {
-                      "type": "accordion",
-                      "title": "Accordion titulo",
-                      "subtitle": "Accordion subtitulo",
-                      "image": {
+            "richContent": [
+                [{
+                    "type": "accordion",
+                    "title": "Accordion titulo",
+                    "subtitle": "Accordion subtitulo",
+                    "image": {
                         "src": {
-                          "rawUrl": "https://example.com/images/logo.png"
+                            "rawUrl": "https://example.com/images/logo.png"
                         }
-                      },
-                      "text": "Aqui se pone el texto "
-                    }
-                  ]
-                ] 
+                    },
+                    "text": "Aqui se pone el texto "
+                }]
+            ]
         }
 
-        agent.add ( new dfff.Payload(agent.UNSPECIFIED , payloadData,{
-           sendAsMessage: true,
-           rawPayload: true 
-        }))      
+        agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {
+            sendAsMessage: true,
+            rawPayload: true
+        }))
     }
 
     //Ejemplo de mensajes capturando entidad del
-    function getQuestionYes(agent){
-      var parametros = req.body.queryResult.parameters;
-      console.log("respuesta: "+JSON.stringify(parametros));
-      var afirmativo = parametros.afirmativo;
-      agent.context.set({'name': 'getQuestion - yes', 'lifespan': 1, 'parameters': {'afirmativo': afirmativo}});
-      agent.add("Todo bien");
+    function getQuestionYes(agent) {
+        var parametros = req.body.queryResult.parameters;
+        console.log("respuesta: " + JSON.stringify(parametros));
+        var afirmativo = parametros.afirmativo;
+        agent.context.set({ 'name': 'getQuestion - yes', 'lifespan': 1, 'parameters': { 'afirmativo': afirmativo } });
+        agent.add("Todo bien");
     }
 
-    function getQuestionNo(agent){
-      var parametros = req.body.queryResult.parameters;
-      console.log("respuesta: "+JSON.stringify(parametros)); 
-      var negativo = parametros.negativo;
-      agent.context.set({'name': 'getQuestion - no', 'lifespan': 1, 'parameters': {'negativo': negativo}});
-      agent.add("Todo mal, Es una pena");
+    function getQuestionNo(agent) {
+        var parametros = req.body.queryResult.parameters;
+        console.log("respuesta: " + JSON.stringify(parametros));
+        var negativo = parametros.negativo;
+        agent.context.set({ 'name': 'getQuestion - no', 'lifespan': 1, 'parameters': { 'negativo': negativo } });
+        agent.add("Todo mal, Es una pena");
     }
+
+    /*
+    function sendEmailHandler(agent) {
+        const { email, name } = agent.parameters;
+        console.log(email);
+        const mailOptions = {
+            from: "Jose Franco Nieto", // sender address
+            to: email, // list of receivers
+            subject: "Email para bots", // Subject line
+            html: `<p> hola ${name}. Esto es un email de prueba </p>`
+        };
+
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+
+            agent.add("Email enviado");
+        });
+
+    }
+    */
 
 
     //-------------------------------
@@ -74,6 +105,7 @@ app.post('/',express.json(), (req,res)=>{
     intentMap.set('customPayloadDemo', customPayloadDemo);
     intentMap.set('getQuestion - yes', getQuestionYes);
     intentMap.set('getQuestion - no', getQuestionNo);
+    intentMap.set('sendEmail', sendEmailHandler);
     //
     agent.handleRequest(intentMap);
 })
@@ -82,12 +114,12 @@ app.post('/',express.json(), (req,res)=>{
 const port = 3000;
 const dateTime = Date.now();
 
-app.listen(process.env.PORT || port, function () {
-  console.log('\x1b[36m'+'------------------------------------------------ ');
-  console.log('\x1b[35m'+'          ** Puerto '+(process.env.PORT || port +' activo **'));
-  console.log('\x1b[0m'+" [ "+Date()+" ] ");
-  console.log(`  Ejemplo escuchando en: ( http://localhost:${port} )`);
-  console.log('\x1b[36m'+'------------------------------------------------ ');
-  console.log('\x1b[0m');
+app.listen(process.env.PORT || port, function() {
+    console.log('\x1b[36m' + '------------------------------------------------ ');
+    console.log('\x1b[35m' + '          ** Puerto ' + (process.env.PORT || port + ' activo **'));
+    console.log('\x1b[0m' + " [ " + Date() + " ] ");
+    console.log(`  Ejemplo escuchando en: ( http://localhost:${port} )`);
+    console.log('\x1b[36m' + '------------------------------------------------ ');
+    console.log('\x1b[0m');
 });
 //Correr en nodemon --> npm run dev
