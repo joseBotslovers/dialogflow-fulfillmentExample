82d5176d6986aedf4569a1ebe531bfb7a4c74f8f
José <veganum@hotmail.com>
2020-11-29 19:27:40 +0100

    funcion insertar en bbdd

--- db/bbdd.js
+++ db/bbdd.js
@@ -1,12 +1,48 @@
 const mysql = require('mysql');
 
+var mysql_pool = mysql.createPool({
+    connectionLimit: 100,
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'dialogflowdb',
+});
+
+function ejecutaQuery(q) {
+    return new Promise((resolve, reject) => {
+        mysql_pool.getConnection((err, con) => {
+            if (err) {
+                con.release();
+                console.log(' Error getting mysql_pool connection: ' + err);
+                throw err;
+            }
+            con.query(q, function(err2, result, fields) {
+                if (err2) {
+                    console.log(' Error in the query: ' + err);
+                    throw err2
+                };
+                resolve(result);
+                con.release();
+            });
+        });
+    });
+}
+
+module.exports = {
+    ejecutaQuery: ejecutaQuery
+}
+
+
+
+
+/*
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "dialogflowdb"
 });
-
+*/
 //CREAR BBDD
 /*con.connect(function(err) {
     if (err) throw err;
@@ -21,16 +57,18 @@ var con = mysql.createConnection({
 
 });
 */
-
+/*
 //CREAR TABLA
 //https://www.w3schools.com/nodejs/nodejs_mysql_create_table.asp
 con.connect(function(err) {
     if (err) throw err;
     console.log("Table Connected!");
-    /*Create a table named "customers":*/
-    var sql = "CREATE TABLE tablechat (name VARCHAR(255), address VARCHAR(255))";
+    //Create a table named "customers":
+    var sql = "CREATE TABLE login (user VARCHAR(255), password VARCHAR(255))";
     con.query(sql, function(err, result) {
         if (err) throw err;
         console.log("Tabla creada");
     });
 });
+
+*/
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const nodemailer = require("nodemailer");
 const axios = require('axios');
 
 const bbdd = require('./db/bbdd');
+
 //Mensaje principal endpoint
 app.get('/', (req, res) => {
     //res.send("¡¡En directoo!!")
@@ -15,142 +16,165 @@ app.get('/', (req, res) => {
 
 app.post('/', express.json(), (req, res) => {
 
-        const agent = new dfff.WebhookClient({
-            request: req,
-            response: res
-        });
+    const agent = new dfff.WebhookClient({
+        request: req,
+        response: res
+    });
 
-        //Para ver cabeceras
-        //console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
-        //Para ver el body
-        //console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
+    //Para ver cabeceras
+    //console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
+    //Para ver el body
+    //console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
 
 
-        //Ejemplo de mensaje simple
-        function demo(agent) {
-            agent.add("Enviando respuesta al Wekhook server");
+    //Ejemplo de mensaje simple
+    function demo(agent) {
+        agent.add("Enviando respuesta al Wekhook server");
+    }
+
+    //Ejemplo de mensaje enriquecido
+    function customPayloadDemo(agent) {
+        var payloadData = {
+            "richContent": [
+                [{
+                    "type": "accordion",
+                    "title": "Accordion titulo",
+                    "subtitle": "Accordion subtitulo",
+                    "image": {
+                        "src": {
+                            "rawUrl": "https://example.com/images/logo.png"
+                        }
+                    },
+                    "text": "Aqui se pone el texto "
+                }]
+            ]
         }
 
-        //Ejemplo de mensaje enriquecido
-        function customPayloadDemo(agent) {
-            var payloadData = {
-                "richContent": [
-                    [{
-                        "type": "accordion",
-                        "title": "Accordion titulo",
-                        "subtitle": "Accordion subtitulo",
-                        "image": {
-                            "src": {
-                                "rawUrl": "https://example.com/images/logo.png"
-                            }
-                        },
-                        "text": "Aqui se pone el texto "
-                    }]
-                ]
+        agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {
+            sendAsMessage: true,
+            rawPayload: true
+        }))
+    }
+
+    //Ejemplo de mensajes capturando entidad del intent
+    function getQuestionYes(agent) {
+        var parametros = req.body.queryResult.parameters;
+        console.log("respuesta: " + JSON.stringify(parametros));
+        var afirmativo = parametros.afirmativo;
+        agent.context.set({ 'name': 'getQuestion - yes', 'lifespan': 1, 'parameters': { 'afirmativo': afirmativo } });
+        agent.add("Todo bien");
+    }
+
+    function getQuestionNo(agent) {
+        var parametros = req.body.queryResult.parameters;
+        console.log("respuesta: " + JSON.stringify(parametros));
+        var negativo = parametros.negativo;
+        agent.context.set({ 'name': 'getQuestion - no', 'lifespan': 1, 'parameters': { 'negativo': negativo } });
+        agent.add("Todo mal, Es una pena");
+    }
+
+    //Nodemailer
+    //Es conveniente en google/seguridad habilitar la opcion de "Acceso de aplicaciones poco seguras"
+    //https://myaccount.google.com/u/5/security
+    var transport = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: "fulgencioPruebas@gmail.com",
+            pass: "Bots.ia77"
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+
+    function sendEmailHandler(agent) {
+        const { email, name } = agent.parameters;
+        console.log(email);
+        //Para el mail que se va a enviar
+        const mailOptions = {
+            from: '"José Franco Nieto 👻" <foo@example.com>',
+            to: email,
+            subject: "Email para bots",
+            text: "Hello world?",
+            html: `<p> hola ${name}. Esto es un email de prueba </p>`
+        };
+        //controlar errores
+        transport.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent: ' + info.response);
+                console.log("Mensaje enviado a: %s", info.messageId);
             }
 
-            agent.add(new dfff.Payload(agent.UNSPECIFIED, payloadData, {
-                sendAsMessage: true,
-                rawPayload: true
-            }))
-        }
-
-        //Ejemplo de mensajes capturando entidad del intent
-        function getQuestionYes(agent) {
-            var parametros = req.body.queryResult.parameters;
-            console.log("respuesta: " + JSON.stringify(parametros));
-            var afirmativo = parametros.afirmativo;
-            agent.context.set({ 'name': 'getQuestion - yes', 'lifespan': 1, 'parameters': { 'afirmativo': afirmativo } });
-            agent.add("Todo bien");
-        }
-
-        function getQuestionNo(agent) {
-            var parametros = req.body.queryResult.parameters;
-            console.log("respuesta: " + JSON.stringify(parametros));
-            var negativo = parametros.negativo;
-            agent.context.set({ 'name': 'getQuestion - no', 'lifespan': 1, 'parameters': { 'negativo': negativo } });
-            agent.add("Todo mal, Es una pena");
-        }
-
-        //Nodemailer
-        //Es conveniente en google/seguridad habilitar la opcion de "Acceso de aplicaciones poco seguras"
-        //https://myaccount.google.com/u/5/security
-        var transport = nodemailer.createTransport({
-            service: 'Gmail',
-            auth: {
-                user: "fulgencioPruebas@gmail.com",
-                pass: "Bots.ia77"
-            },
-            tls: {
-                rejectUnauthorized: false
-            }
         });
 
+        console.log("correo enviado");
 
-        function sendEmailHandler(agent) {
-            const { email, name } = agent.parameters;
-            console.log(email);
-            //Para el mail que se va a enviar
-            const mailOptions = {
-                from: '"José Franco Nieto 👻" <foo@example.com>',
-                to: email,
-                subject: "Email para bots",
-                text: "Hello world?",
-                html: `<p> hola ${name}. Esto es un email de prueba </p>`
-            };
-            //controlar errores
-            transport.sendMail(mailOptions, function(error, info) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                    console.log("Mensaje enviado a: %s", info.messageId);
-                }
-
-            });
-
-            console.log("correo enviado");
-
-            agent.add(`Tu correo ha sido enviado ${name} por favor mira la bandeja de tu correo`);
-        }
+        agent.add(`Tu correo ha sido enviado ${name} por favor mira la bandeja de tu correo`);
+    }
 
 
-        //Usar Excel como BBDD con sheetDB y axios para las peticiones http
-        function saveOrder(agent) {
-            const pizza = agent.parameters.menu;
-            var d = Date();
-            fecha = d.toString()
+    //Usar Excel como BBDD con sheetDB y axios para las peticiones http
+    function saveOrder(agent) {
+        const pizza = agent.parameters.menu;
+        var d = Date();
+        fecha = d.toString()
 
-            axios.post('https://sheetdb.io/api/v1/pikq7r2f1d3xu', {
-                "data": { "name": pizza, "created_at": fecha }
-            }).then(res => {
+        axios.post('https://sheetdb.io/api/v1/pikq7r2f1d3xu', {
+            "data": { "name": pizza, "created_at": fecha }
+        }).then(res => {
+            console.log(res.data);
+        });
+
+        axios.get('https://sheetdb.io/api/v1/pikq7r2f1d3xu')
+            .then(res => {
                 console.log(res.data);
             });
 
-            axios.get('https://sheetdb.io/api/v1/pikq7r2f1d3xu')
-                .then(res => {
-                    console.log(res.data);
-                });
+        agent.add(`Okay, tu ` + pizza + ` estara lista en 40 minutos. `);
 
-            agent.add(`Okay, tu ` + pizza + ` estara lista en 40 minutos. `);
+    }
 
-        }
 
-        //-------------------------------------------------------------------------------------
-        //Conexion de intent con la funccion con la que van a interactuar
-        var intentMap = new Map();
-        //Intent y funcion para interactuar
-        intentMap.set('webhookDemo', demo);
-        intentMap.set('customPayloadDemo', customPayloadDemo);
-        intentMap.set('getQuestion - yes', getQuestionYes);
-        intentMap.set('getQuestion - no', getQuestionNo);
-        intentMap.set('sendEmail', sendEmailHandler);
-        intentMap.set('Order a pizza', saveOrder);
-        //
-        agent.handleRequest(intentMap);
-    })
+    async function loginInsert(agent) {
+        const user = agent.parameters.user;
+        const password = agent.parameters.password;
+        console.log("usuario: " + user + " contraseña: " + password);
+
+        insert_BBDD(user, password);
+
+        agent.add("usuario: " + user + " contraseña: " + password);
+        agent.add("Registro completado");
+
+    }
+
+
     //-------------------------------------------------------------------------------------
-    // ARRANCAMOS EL SERVICIO
+    //Conexion de intent con la funccion con la que van a interactuar
+    var intentMap = new Map();
+    //Intent y funcion para interactuar
+    intentMap.set('webhookDemo', demo);
+    intentMap.set('customPayloadDemo', customPayloadDemo);
+    intentMap.set('getQuestion - yes', getQuestionYes);
+    intentMap.set('getQuestion - no', getQuestionNo);
+    intentMap.set('sendEmail', sendEmailHandler);
+    intentMap.set('Order a pizza', saveOrder);
+    intentMap.set('login_Registrar', loginInsert);
+    //
+    agent.handleRequest(intentMap);
+})
+
+
+async function insert_BBDD(user, password) {
+    var q = `INSERT INTO login (user, password) VALUES ('${user}','${password}')`;
+    console.log(q);
+    await bbdd.ejecutaQuery(q);
+}
+
+//-------------------------------------------------------------------------------------
+// ARRANCAMOS EL SERVICIO
 const port = 3000;
 const dateTime = Date.now();
 
